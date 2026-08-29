aeb858c7d21b44ef177f0aaadf1a1e7ae814e0b0
Gurrasso <92580760+Gurrasso@users.noreply.github.com>
2026-08-29 20:25:51 +0200

    no comment

--- scriptable/lockscreen-widget.js
+++ scriptable/lockscreen-widget.js
@@ -1,2 +1,14 @@
-var widget = new ListWidget()
+// Lock Screen widget
+let widget = new ListWidget()
+
+let time = new Date().toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit"
+})
+
+let text = widget.addText(time)
+text.font = Font.systemFont(18)
+text.textColor = Color.white()
+
 Script.setWidget(widget)
+Script.complete()
