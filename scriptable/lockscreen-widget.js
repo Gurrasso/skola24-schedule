d5ba8a4616be9a1fd7347211869964dc86438349
Gurrasso <92580760+Gurrasso@users.noreply.github.com>
2026-08-29 20:45:12 +0200

    Update lockscreen-widget.js

--- scriptable/lockscreen-widget.js
+++ scriptable/lockscreen-widget.js
@@ -1,32 +1,29 @@
-lesson = {
-	"name" : "Matte",
-	"teachers" : ["AFo"],
-	"rooms" : ["C32"],
-	"start" : "10:00",
-	"end" : "11:00"
+const lesson = {
+	name: "Matte",
+	teachers: ["AFo"],
+	rooms: ["C32"],
+	start: "10:00",
+	end: "11:00"
 }
+
+let widget = new ListWidget()
 
 build_widget(lesson)
 
-function arr_to_string(arr){
- let returnString = ''
- arr.forEach((item, index) => {
-   if(index !== 0) returnString += '|'
-   returnString += `${item.name}:${item.value}`
- })
- return returnString
+function arr_to_string(arr) {
+	return arr.join(", ")
 }
 
-function build_widget(lesson){
-	let widget = new ListWidget()
-
+function build_widget(lesson) {
 	let name = widget.addText(lesson.name)
 	name.font = Font.boldSystemFont(12)
-	
-	let time = widget.addText(lesson.start, "	", lesson.end)
+
+	let time = widget.addText(`${lesson.start} - ${lesson.end}`)
 	time.font = Font.systemFont(12)
 
-	let teachers_rooms = widget.addText(arr_to_string(lesson.teachers), "	", arr_to_string(lesson.rooms))
+	let teachers_rooms = widget.addText(
+		`${arr_to_string(lesson.teachers)}		${arr_to_string(lesson.rooms)}`
+	)
 	teachers_rooms.font = Font.systemFont(12)
 }
 
