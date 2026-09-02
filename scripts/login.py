e8c86ed43bff4791f20b2fa7b597a48d8f98860a
Gurrasso <svengunnarlonnqvist@gmail.com>
2026-09-02 13:59:22 +0200

    Update login.py

--- scripts/login.py
+++ scripts/login.py
@@ -1,12 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-
-    context = browser.new_context()
-    page = context.new_page()
-
-    # TODO: add support for different municipalities
+def login_lund_sso(page):
     page.goto("https://lund-sso.skola24.se/")
 
     # Enter username or email manually
@@ -21,14 +15,51 @@ with sync_playwright() as p:
     ).click()
 
     # Enter your password manually
-    page.get_by_role("textbox").fill(input("Enter password: "))
+    page.get_by_role("textbox").fill(
+        input("Enter password: ")
+    )
     page.get_by_role("button", name="Sign in").click()
 
-    page.wait_for_load_state("networkidle")
+def login_default(page, domain):
+    page.goto("https://" + domain + ".skola24.se/")
 
-    # Save cookies/local storage/etc.
-    context.storage_state(path="auth.json")
+    # Enter username manually
+    page.get_by_role("textbox", name="Användarnamn").fill(
+        input("Enter Username: ")
+    )
 
-    print("Authentication state saved to auth.json")
+    # Enter your password manually
+    page.get_by_role("textbox", name="Lösenord").fill(
+        input("Enter password: ")
+    )
 
-    browser.close()
+    page.get_by_role("button", name="Logga in").click()
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+
+        context = browser.new_context()
+        page = context.new_page()
+
+        # TODO: Add check for invalid domains
+        domain = input("Enter skola24 domain(municipality): ").lower()
+        match domain:
+            case "":
+                print("Invalid domain")
+                return
+            case "lund" | "lund-sso":
+                login_lund_sso(page)
+            case _:
+                login_default(page, domain)
+
+        page.wait_for_load_state("networkidle")
+
+        # Save cookies/local storage/etc.
+        context.storage_state(path="auth.json")
+
+        print("Authentication state saved to auth.json")
+
+        browser.close()
+
+main()
